d35c5e6a554c26d973453627fe0334486fe9f4f8
Ramudgar <ramudgary52@gmail.com>
2025-02-10 10:26:27 +0545

    form is made

---
+++ javaScript/factorial.js
@@ -0,0 +1,20 @@
+// write a program to find the factorial of a given number using loop
+
+// function factorial(num) {
+//     let result = 1;
+//     for (let i = 1; i <= num; i++) {
+//         result *= i; // result = result * i;
+//     }
+//     return result;
+// }
+
+// const result = factorial(5);
+// console.log(result); // 120
+
+let i = 2;
+while (i <= 10) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+  i++;
+}
